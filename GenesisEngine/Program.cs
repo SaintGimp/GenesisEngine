70e062a47471d7e4003fb8ffdea332e3b33af5a5
Eric Lee <saintgimp@hotmail.com>
2017-07-17 14:21:33 -0700

    Update dependencies

--- GenesisEngine/Program.cs
+++ GenesisEngine/Program.cs
@@ -15,6 +15,9 @@ namespace GenesisEngine
 
             using (var game = Bootstrapper.Container.GetInstance<Genesis>())
             {
+                // At the moment MonoGame 3.6 doesn't support SharpDX 4, e.g. https://github.com/MonoGame/MonoGame/issues/3794
+                // so if you crash here with an error that SharpDX types aren't found, that's probably the issue. I think this
+                // will be addressed in MonoGame 3.7.
                 game.Run();
             }
         }
